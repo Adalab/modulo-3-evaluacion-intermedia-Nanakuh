03a73c8bd3eed46e2b581b4cf00ca95d9ab0a0f3
Ana Gil <anagilrodrig@gmail.com>
2024-02-03 11:04:17 +0100

    Trying select

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,12 +1,16 @@
 function Filters () {
   return (
-    <form className="filters">
-    <label htmlFor="Quote" className="filters_label"> Frases</label>
-    <input className="filter_input" type="text" placeholder="Buscar frase..."  />
-    
-    <label htmlFor="Character"className="filters_label"> Personje</label>
-    <input className="filter_input" type="text" placeholder="Buscar por personaje..."  />
-  </form>
+    <div className="form">
+      <form className="filters">
+      <label htmlFor="Quote" className="filters_label"> Frases</label>
+      <input className="filter_input" type="text" placeholder="Buscar frase..."  />
+      
+      <label htmlFor="Character"className="filters_label"> Personje</label>
+      <input className="filter_input" type="text" placeholder="Buscar por personaje..."  />
+        <option value="Mike"></option>
+        <option value="Ross"></option>
+      </form>
+  </div>
   );
 }
 export default Filters;
